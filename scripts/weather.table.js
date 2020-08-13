5e48f44e5f258b7c5b2ab601e9594561c625c80a
Igor <kuznetsoviv1992@yandex.ru>
2020-08-14 01:32:08 +0300

    second version

--- scripts/weather.table.js
+++ scripts/weather.table.js
@@ -1,10 +1,8 @@
 'use strict'
 
-const e = React.createElement
-
 const ID = 'city_name'
 
-const API_KEY = '34340d52684a482386b3a9768a98c0e3'
+const API_KEY = '38e10902973345ba92f96022da41797c'
 const URL = 'https://api.weatherbit.io/v2.0/current'
 const REQUEST_TEMPLATE = `${URL}?lang=ru&key=${API_KEY}&city=`
 
@@ -30,92 +28,106 @@ class WeatherTable extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { data: new Map() }
+    this.state = { data: new Map(), offeredCities: [] }
     if (Array.isArray(props.cities)) {
-      props.cities.forEach(city => this.fillData(city))
+      props.cities.forEach(city => this.fetchData(city))
     }
+    this.searchInput = React.createRef()
   }
 
-  fillData(city) {
-    let xhr = new XMLHttpRequest()
-    xhr.weatherTable = this
-    xhr.open('GET', `${REQUEST_TEMPLATE}${city}`, true)
-    xhr.send()
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState != 4) return
-      if (xhr.status != 200) {
-        alert(`Невозможно получить данные для города ${city}`)
-        console.log(xhr.status + ': ' + xhr.statusText)
-      } else {
-        let response = JSON.parse(xhr.responseText)
-        if (Array.isArray(response['data'])) {
-          let data = xhr.weatherTable.state.data
-          response['data']
-            .filter(row => row[ID])
-            .forEach(row => data.set(row[ID], row))
-          xhr.weatherTable.setState({ data: data })
-        }
-      }
+  fetchData(city) {
+    fetch(`${REQUEST_TEMPLATE}${city}`)
+      .then(response => response.json())
+      .then(response => {
+        let data = this.state.data
+        response['data']
+          .filter(row => row[ID])
+          .forEach(row => data.set(row[ID], row))
+        this.setState({ data: data })
+      })
+      .catch(error => alert(`Невозможно получить данные для города ${city}`))
+  }
+
+  filterOfferedCities(event) {
+    let searchString = event.target.value
+    if (searchString.length > 1) {
+      searchCities(searchString).then(result => {
+        this.setState({ offeredCities: result })
+      })
     }
+    this.setState({ offeredCities: [] })
+  }
+
+  addCity(event) {
+    this.setState({ offeredCities: [] })
+    this.fetchData(event.target.text)
+    this.searchInput.current.value = ""
   }
 
   renderTableHeader() {
-    return e('tr', null,
-      WeatherTable.headerValues.map((value, index) =>
-        e('th', { key: index }, value)
-      ),
-      e('th', null, 'Удалить')
+    return (
+      <tr>
+        {WeatherTable.headerValues.map((value, index) => (
+          <th key={index}>{value}</th>
+        ))}
+        <th>Удалить</th>
+      </tr>
     )
   }
 
   renderTableData() {
-    let rows = Array.from(this.state.data.values())
-    return rows.map(row => {
-      return e('tr', { key: row[ID] },
-        WeatherTable.headerKeys.map((key, index) =>
-          e('td', { key: index }, row[key])
-        ),
-        e('td', null, this.renderDeleteButton(row[ID]))
+    return Array.from(this.state.data.values()).map(row => {
+      return (
+        <tr key={row[ID]}>
+          {WeatherTable.headerKeys.map((key, index) => (
+            <td key={index}>{row[key]}</td>
+          ))}
+          <td>{this.renderDeleteButton(row[ID])}</td>
+        </tr>
       )
     })
   }
 
   renderDeleteButton(cityName) {
-    return e('button', {
-      onClick: () => {
-        let data = this.state.data
-        data.delete(cityName)
-        this.setState({ data: data })
-      },
-      className: 'btn removebtn'
-    })
-  }
-
-  renderAddButton() {
-    return e('tr', { key: 'last' },
-      e('td', { colspan: WeatherTable.columnsSize },
-        e('button', {
-          onClick: () => {
-            let cityName = prompt('Введите наименование города', 'Moscow')
-            if (cityName) {
-              this.fillData(cityName)
-            }
-          },
-          className: 'btn addbtn'
-        })
-      )
+    return (
+      <button
+        onClick={() => {
+          let data = this.state.data
+          data.delete(cityName)
+          this.setState({ data: data })
+        }}
+        className="remove-btn"
+      ></button>
     )
   }
 
   render() {
-    return e('div', null,
-      e('table', { id: 'weather' },
-        e('tbody', null,
-          this.renderTableHeader(),
-          this.renderTableData(),
-          this.renderAddButton()
-        )
-      )
+    return (
+      <div>
+          <div class="dropdown-content">
+            <input
+              ref={this.searchInput}
+              class="search-input"
+              type="text"
+              placeholder="Добавить город"
+              title="Введите 2 символа для начала поиска"
+              onKeyUp={() => this.filterOfferedCities(event)}
+            ></input>
+            <div class="selectedOptions">
+              {this.state.offeredCities.map(key => (
+                <a key={key} onClick={() => this.addCity(event)}>
+                  {key}
+                </a>
+              ))}
+            </div>
+        </div>
+        <table>
+          <tbody>
+            {this.renderTableHeader()}
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
